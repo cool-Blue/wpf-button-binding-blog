2833930f108df710af5d9cae48adbb22298dd532
cool-Blue <cool.blue@y7mail.com>
2016-11-19 20:23:10 +1100

    init

---
+++ ViewModels/ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls.Primitives;
+using ControlAutomation;
+
+namespace ButtonBindingMinimal.ViewModels
+{
+	/// <summary>
+	/// mock model
+	/// This is a remote control interface bound to a button on the view
+	/// </summary>
+	public class ViewModel
+	{
+		public ToggleAutomation TargetButton { get; set; }
+
+		public ViewModel (ToggleButton tb)
+		{
+			TargetButton = new ToggleAutomation(tb);
+		}
+	}
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using ButtonBindingMinimal.ViewModels;
+
+namespace ButtonBindingMinimal.Views
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public StaticButton Button { get; set; }
+		public StaticButton Remote { get; set; }
+		public MainWindow ()
+		{
+			InitializeComponent();
+			Button = new StaticButton(new List<string> { "Checked State", "UnChecked State" }, true);
+			Remote = new StaticButton(new List<string> { "", "Remote" }, false);
+
+			// build the view model and connect it to the view
+			DataContext = new ViewModel(target);
+		}
+	}
+}
